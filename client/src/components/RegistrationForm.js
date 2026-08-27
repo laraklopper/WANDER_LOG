169df10c25a5fd1a0b2421f983bb84ea3202fbbe
Lara Klopper <laraK@corprecover.co.za>
2026-08-27 15:55:21 +0200

    changes

---
+++ client/src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function RegistrationForm() {
+  return (
+    <form>
+        <p className='visually-hidden' id='formTitle'>REGISTRATION FORM</p>
+        <div id='formHeadingBlock'>
+            <h3 id='formHeading'>SIGN UP</h3>
+        </div>
+    </form>
+  )
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import '../css/pagesCss/PageSetup.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import MainHeader from '../components/MainHeader'
+import '../css/componentCss/FormSetup.css'
+import RegistrationForm from '../components/RegistrationForm';
 export default function Register() {
   return (
     <div id='pageContainer'>
       <MainHeader mainHeading={'REGISTER'}/>
+      <section id='regis-section1'>
+   <Row id='register-row'>
+      <Col/>
+        <Col xs={12} md={8}>
+          <div id='regis-form-panal'>
+            <RegistrationForm/>
+          </div>
+          
+        </Col>
+        <Col/>
+      </Row>
+      </section>
+   
     </div>
   )
 }
