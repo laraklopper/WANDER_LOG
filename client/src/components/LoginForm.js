d64f10154152a48598bc5a6ef3b680a8939d44c3
Lara Klopper <laraK@corprecover.co.za>
2026-08-29 08:45:50 +0200

    new CSS file

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/componentCss/FormSetup.css'
 import Stack from 'react-bootstrap/Stack';
 export default function LoginForm() {
+    const [showPswdMsg, setShowPswdMsg] = useState(false)
+
   return (
     <form>
+        <div id='formHeadingBlock'>
+            <h3 id='formHeading'>SIGN IN</h3>
+        </div>
+        <div >
+           <Stack gap={3}>
+      <div className="p-2">
+        <label className='login-label'>USERNAME</label>
+        <input
+            className='input'
+            placeholder='USERNAME'
+            required
+            autoComplete='username'
+            // name=''
+            // value={}
+            // ARIA ATTRIBUTES:
+
+        />
+      </div>
+      {/* Error Message */}
+      {/* <div className="p-2">Second item</div> */}
+      {/* <div className="p-2">Third item</div> */}
+    </Stack>
+     <Stack gap={3}>
+      <div className="p-2">
+        <label>PASSWORD:</label>
+        <input
+            className='input'
+            
+        />
+      </div>
+      <div className="p-2">Second item</div>
+      <div className="p-2">Third item</div>
+    </Stack>  
+        </div>
 
     </form>
   )
