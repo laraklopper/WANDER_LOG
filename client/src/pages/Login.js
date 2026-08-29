1603b5cfd5a8a1caeb88fe6a8a4aa133945e3306
Lara Klopper <laraK@corprecover.co.za>
2026-08-29 08:35:50 +0200

    changes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import MainHeader from '../components/MainHeader'
 import '../css/pagesCss/PageSetup.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import MainHeader from '../components/MainHeader'
 export default function Login() {
   return (
     <div id='pageContainer'>
       <MainHeader mainHeading={'LOGIN'}/>
+      <section>
+         <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      </section>
     </div>
   )
 }
