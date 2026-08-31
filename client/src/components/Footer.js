d907a68f6eef5b4d8e4fcb951dd69e32852b9152
Lara Klopper <laraK@corprecover.co.za>
2026-08-31 15:43:11 +0200

    changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,32 @@
 import React from 'react'
+import '../css/componentCss/Footer.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 export default function Footer({logout}) {
   return (
-    <footer>
-    <Row>
-        <Col md={12}></Col>
+    <footer id='footer'>
+    <Row id='footer-row1'>
+        <Col md={12} id='footer-col1'></Col>
     </Row>
-    <Row>
-        <Col xs={12} md={8}>
-          xs=12 md=8
-        </Col>
-        <Col xs={6} md={4}>
-          <Button variant='warning' onClick={logout}>LOGOUT</Button>
+    <Row id='footer-row2'>
+        <Col xs={12} md={8} id='logout-col1'></Col>
+        <Col xs={6} md={4} id='logout-col2'>
+          <Button 
+            variant='warning' 
+            onClick={logout} 
+            id='logoutBtn' 
+            type='button'
+            >
+            LOGOUT
+          </Button>
         </Col>
       </Row>
-         <Row>
-        <Col>1 of 3</Col>
-        <Col xs={6}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
+         <Row id='footer-row3'>
+        <Col/>
+        <Col xs={6}></Col>
+        <Col/>
       </Row>
     </footer>
   )
