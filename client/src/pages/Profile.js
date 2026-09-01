7402794afdfe30f74522e23855c8e327770daf3d
Lara Klopper <laraK@corprecover.co.za>
2026-09-01 12:49:39 +0200

    changes

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,13 +8,22 @@ import Stack from 'react-bootstrap/Stack';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import EditPasswordForm from '../components/EditPasswordForm';
+import EditUserForm from '../components/EditUserForm';
 
 export default function Profile({currentUser, logout}) {
+  const [showEditProfileForm, setShowEditProfileForm] = useState(false);
   const [showEditPswdForm, setShowEditPswdForm] = useState(false);
 
+  const toggleEditProfile = useCallback(() => {
+    setShowEditProfileForm(prevState => !prevState);
+    setShowEditPswdForm(false); // Ensure the edit password form is hidden when toggling the profile form
+  }, []);
   const toggleEditPswd = useCallback(() => {
     setShowEditPswdForm(prevState => !prevState);
+    setShowEditProfileForm(false); // Ensure the edit profile form is hidden when toggling the password form
   }, []);
+
+
   return (
     <div id='pageContainer'>
       <Header currentUser={currentUser} heading={'PROFILE'}/>
@@ -23,16 +32,38 @@ export default function Profile({currentUser, logout}) {
           <Row id='toggleEditProfileRow'>
         <Col id='toggleEditProfileCol1'/>
         <Col xs={5} id='toggleEditProfileCol'>
-          <Stack gap={3} id='toggleEditProfileStack'>
+          <Stack gap={3} id='toggleEditProfileFormsStack'>
           {/* EDIT PROFILE FORM BUTTON */}
-          <Button variant="light" id='editUserToggleBtn'>
-            Edit Profile
+          <Button 
+          variant="light" 
+          id='editUserToggleBtn'
+          onClick={toggleEditProfile}
+          type="button"
+          // ARIA ATTRIBUTES
+          aria-expanded={showEditProfileForm}
+          aria-controls="edit-profile-panal"
+          aria-label={showEditProfileForm ? 'Close Edit Profile Form' : 'Edit Profile'}
+          aria-describedby="edit-profile-panal"
+          aria-pressed={showEditProfileForm}
+          >
+            {showEditProfileForm ? 'Close Edit Profile Form' : 'Edit Profile'}
           </Button>
       <div className="p-2"></div>
       {/* TOGGLE EDIT PASSWORD FORM BUTTON */}
       <div className="p-2">
-        <Button variant="light" id='editPswdToggleBtn' onClick={toggleEditPswd}>
-          Edit Password
+        <Button 
+          variant="light" 
+          id='editPswdToggleBtn' 
+          onClick={toggleEditPswd}
+          type="button"
+          // ARIA ATTRIBUTES
+          aria-expanded={showEditPswdForm}
+          aria-controls="edit-password-panal"
+          aria-label={showEditPswdForm ? 'Close Edit Password Form' : 'Edit Password'}
+          aria-describedby="edit-password-panal"
+          aria-pressed={showEditPswdForm}
+          >
+          {showEditPswdForm ? 'Close Edit Password Form' : 'Edit Password'}
         </Button>
       </div>
     </Stack>
@@ -40,6 +71,17 @@ export default function Profile({currentUser, logout}) {
         <Col id='toggleEditProfileCol2'/>
       </Row>
         </div>
+        {showEditProfileForm && (
+          <div id='edit-profile-panal'>
+            <Row id='edit-profile-row'>
+              <Col id='edit-profile-col1'/>
+              <Col md={10} id='edit-profile-col'>
+                <EditUserForm/>
+              </Col>
+              <Col id='edit-profile-col2'/>
+            </Row>
+          </div>
+        )}
         {showEditPswdForm && (
           <div id='edit-password-panal'>
             <Row id='edit-password-row'>
