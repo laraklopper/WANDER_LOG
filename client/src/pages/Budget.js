7dff2ea43c9bc613e93fa8266eb29f7a271b4f0b
Lara Klopper <laraK@corprecover.co.za>
2026-09-02 09:04:18 +0200

    changes

--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -7,18 +7,32 @@ import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import CurrencyConverter from '../components/CurrencyConverter';
 export default function Budget({currentUser, logout}) {
-  const [toggleCalc, setToggleCalc] = useState(false)
-  const [showConverter, setShowConverter] = useState(false)
+  // ==========STATE VARIABLES===============
+  // Toggle Buttons State
+  const [showCalculator, setShowCaculator] = useState(false)
   const [showVatCalc, setShowVatCalc] = useState(false)
-
+  const [showConverter, setShowConverter] = useState(false)
+  
+  //================EVENT LISTENERS========================
+  //  Function to toggle general/number calculator
   const toggleCalculator = useCallback(() => {
-
+    setShowCaculator(prev => !prev)
+    setShowVatCalc(false)
+    setShowConverter(false)
   },[])
+  //  Function to toggle Vat calculator
   const toggleVatCalculator = useCallback(() => {
-
+    setShowVatCalc(prev => !prev)
+    setShowCaculator(false)
+    setShowConverter(false)
   },[])
+  //  Function to toggle currency converter
   const toggleConverter = useCallback(() => {
+    setShowConverter(prev => !prev)
+    setShowCaculator(false)
+    setShowVatCalc(false)
 
   },[])
 
@@ -33,21 +47,50 @@ export default function Budget({currentUser, logout}) {
         <Col xs={5} id='toggle-col'>
           <Stack gap={1} id='toggle-btns-stack'>
       <div id='toggle-calculator-block'>
-        <Button>
+        <Button onClick={toggleCalculator}>
           SHOW CALCULATOR
         </Button>
       </div>
       <div id='toggle-intcalculator-block'>
-        <Button variant='light'>SHOW VAT CALCULATOR</Button>
+        <Button variant='light' onClick={toggleVatCalculator}>SHOW VAT CALCULATOR</Button>
       </div>
       <div id='toggle-converter-block'>
-        <Button>SHOW CURRENCY CONVERTER</Button>
+        <Button onClick={toggleConverter}>SHOW CURRENCY CONVERTER</Button>
       </div>
     </Stack>
         </Col>
         <Col id='toggle-col2'/>
       </Row>
+      {showCalculator && (
+        <div id='basic-calculator-panal'>
+          <Row id='basic-calculator-row'>
+        <Col/>
+        <Col xs={5}></Col>
+        <Col />
+      </Row>
+        </div>
+      )}
+      {showVatCalc && (
+        <div id='vat-calculator-panal'>
+          <Row id='vat-calculator-row'>
+            <Col id='vat-calculator-col1'/>
+            <Col xs={12} md={8} id='vat-calculator-col'></Col>
+            <Col id='vat-calculator-col2'/>
+          </Row>
+        </div>
+      )}
+      {showConverter && (
+        <div id='currency-converter-panal'>
+        <Row id='currency-converter-row'>
+        <Col id='currency-convert-col1'/>
+        <Col xs={6} id='currency-convert-col'>
+          <CurrencyConverter/>
+        </Col>
+        <Col id='currency-convert-col2'/>
+      </Row>
 
+        </div>
+      )}
           </div>
         </section>
       <Footer logout={logout}/>
