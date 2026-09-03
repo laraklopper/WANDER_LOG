d0b706d75e657655ce369c7c075d29bf7fdf1590
Lara Klopper <laraK@corprecover.co.za>
2026-09-03 13:19:31 +0200

    changes

---
+++ client/src/components/AddTripForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/componentCss/AddTripForm.css'
+import '../css/componentCss/FormSetup.css'
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+
+export default function AddTripForm() {
+  return (
+    <div>AddTripForm</div>
+  )
+}
