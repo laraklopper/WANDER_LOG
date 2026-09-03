80d3c5bd9dd503005013129621f7c91e01f7938e
Lara Klopper <laraK@corprecover.co.za>
2026-09-03 12:49:03 +0200

    changes

---
+++ server/routes/tripRoutes.js
@@ -0,0 +1,12 @@
+// tripRoutes.js
+/* Trip endpoints, mounted at /trip by app.js
+
+- GET /fetchTrips - List current loggedIn users trips 
+- GET /fetchTrip/:id - Get one trip (?with entries)
+- POST /addTrip - create a new trip 
+- PATCH /editTrip/:id - update a trip
+- DELETE /deleteTrip/:id - Delete a single trip by id
+
+all routes require JWT Auth
+*/
+
