1bd64f1dcc7bd9471bc795159cc646c5eafbc0b8
Lara Klopper <laraK@corprecover.co.za>
2026-09-04 12:06:12 +0200

    cssImports

---
+++ client/src/pages/Expenses.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/pagesCss/Expenses.css'
+import '../css/pagesCss/PageSetup.css'
+export default function Expenses() {
+  return (
+    <div>Expenses</div>
+  )
+}
