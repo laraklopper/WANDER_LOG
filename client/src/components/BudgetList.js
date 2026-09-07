d6892aecff9ac8cbd59cb7c307995a58ce7477a0
Lara Klopper <laraK@corprecover.co.za>
2026-09-07 15:08:17 +0200

    changes

---
+++ client/src/components/BudgetList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/componentCss/DetailsPanal.css'
+/*
+TABLE DISPLAY
+- trip
+- base currency
+- total budget 
+- expenses
+
+*/
+
+export default function BudgetList() {
+  return (
+    <div>BudgetList</div>
+  )
+}
