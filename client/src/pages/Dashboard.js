3e4295ae5dea99188c80c5f148ba822b641d9994
Lara Klopper <laraK@corprecover.co.za>
2026-09-08 14:15:18 +0200

    changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,6 +7,7 @@ import '../css/pagesCss/Dashboard.css'
 // IMPORT BOOTSTRAP COMPONENTS
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
 // IMPORT CUSTOM COMPONENTS
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -42,11 +43,69 @@ export default function Dashboard(//Export the default Dashboard.js function com
               <h3 id='dashboardHeading'>Welcome {fullName || 'User'}!</h3>
             </Col>
           </Row>
-          <Row id='dashboard-row2'>
-            <Col md={12} id='dashboard-col2'>
-              
+          <div id='dashBoardProfile'>
+              <Row id='dashboard-row2'>
+               <Col xs={12} md={8} id='dashboardProfile-col1'>
+              <div id='dashboardProfileBody'>
+{/* username , email, admin status */}
+<Stack gap={3} id='dashboardStack'>
+{/* FULL NAME */}
+      <div className="p-2" id='dashboardUsernameBlock1'>
+     
+      <span className='profile-span'>
+        <h5 className='profile-label'>NAME:</h5>
+        <span id='profileFullName'>
+          <h6 className='profile-value'>{currentUser?.fullName?.firstName || 'NOT_AVAILABLE'}</h6>
+          <h6 className='profile-value'> {currentUser?.fullName?.lastName || 'NOT_AVAILABLE'}</h6>
+        </span>
+        
+      </span>
+             
+      
+      </div>
+      {/* USERNAME */}
+      <div className="p-2" id='dashboardProfileBlock2'>
+  <span className='profile-span'>
+          <h5 className='profile-label'>USERNAME:</h5>
+          <h6 className='profile-value'>{currentUser?.username || 'NOT_AVAILABLE'}</h6>
+        </span>
+      </div>
+      {/* EMAIL */}
+      <div className="p-2" id='dashboardProfileBlock3'>
+        <span className='profile-span'>
+          <h5 className='profile-label'>EMAIL:</h5>
+          <h6 className='profile-value'>{currentUser?.email || 'NOT_AVAILABLE'}</h6>
+        </span>
+      </div>
+      {/* ADMIN STATUS */}
+      <div className="p-2" id='dashboardProfileBlock4'>
+        <span className='profile-span'>
+          <h5 className='profile-label'>LOGGED IN AS:</h5>
+          <h6 className='profile-value'>
+            {currentUser?.admin ? 'ADMIN' : 'USER'}
+          </h6>
+        </span>
+      </div>
+    </Stack>
+              </div>
+            </Col>
+            <Col xs={6} md={4} id='dashboardProfile-col2'>
+              {/* PROFILE PICTURE IF SET */}
             </Col>
           </Row>
+          </div>
+         
+        </div>
+      </section>
+      <section id='dashboardSection2'>
+        <div id='dashBoardSec2-panal'>
+          <Row id='dashboardLinkRow'>
+            <Col md={12} id='dashBoardLinkCol'>
+              
+            </Col>
+            
+      </Row>
+
         </div>
       </section>
       <Footer logout={logout}/>
