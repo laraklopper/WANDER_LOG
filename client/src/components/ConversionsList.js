9eae65937d9498d4126b1d7829be2b7b8d1ac543
Lara Klopper <laraK@corprecover.co.za>
2026-09-08 16:00:18 +0200

    conversions

--- client/src/components/ConversionsList.js
+++ client/src/components/ConversionsList.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
-export default function ConversionsList({conversionsTotal, loggedIn, currencyOptions, fetchConversions, setError}) {
+export default function ConversionsList({conversionsTotal, loggedIn,currentUser, currencyOptions, fetchConversions, setError}) {
+  const username = currentUser?.username || 'NOT_AVAILABLE'
   return (
-    <div>ConversionsList</div>
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={5}>
+                {username} : CURRENCY CONVERSIONS
+              </th>
+            </tr>
+            <tr>
+              <th>AMOUNT</th>
+              <th>BASE CURRENCY</th>
+              <th>TARGET CURRENCY</th>
+              <th>RATE</th>
+              {/* TARGET CURRENCY AMOUNT */}
+              <th>AMOUNT</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
   )
 }
