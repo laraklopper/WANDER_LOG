d8ed86973f2b085fcadb26b77463e4d9d5ebb932
Lara Klopper <laraK@corprecover.co.za>
2026-09-09 09:56:31 +0200

    changes

---
+++ client/src/components/EditTripForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/componentCss/EditTrip.css'
+import '../css/componentCss/FormSetup.css'
+
+export default function EditTripForm() {
+  return (
+    <form>
+        <div id='formHeadingBlock'>
+            <h3 id='formHeading'>
+                EDIT TRIP
+            </h3>
+
+        </div>
+    </form>
+  )
+}
