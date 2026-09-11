7d44f0829e59682591291841452ea25ed7f1741e
Lara Klopper <laraK@corprecover.co.za>
2026-09-11 11:16:41 +0200

    changes

--- client/src/components/FilterExpenses.js
+++ client/src/components/FilterExpenses.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import '../css/componentCss/FilterForms.css'
 import '../css/componentCss/FormSetup.css'
-/*Filter expenes by currency, paymentMethod, category, is paid */
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+
 export default function FilterExpenses() {
   return (
     <form id='filterForm'>
+      <div id='filter-form-input'>
+      {/*Filter expenes by currency, paymentMethod, category, is paid  */}
+           <Stack gap={3}>
+      <div className="p-2">
+        <div className='input-div'>
+          <label>CURRENCY:</label>
+          <select>
+            <option>SELECT</option>
+          </select>
+
+        </div>
+      </div>
+      <div className="p-2">Second item</div>
+      <div className="p-2">Third item</div>
+    </Stack>
+
+    <Stack direction="horizontal" gap={3}>
+      <div className="p-2">First item</div>
+      <div className="p-2 ms-auto">Second item</div>
+      <div className="p-2">Third item</div>
+    </Stack>
+      </div>
 
     </form>
   )
