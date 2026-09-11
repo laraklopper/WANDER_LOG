400b4cc2fa09bf2fbe0a8623e01ff1a5cb53c4b3
Lara Klopper <laraK@corprecover.co.za>
2026-09-11 08:53:45 +0200

    ROute planning

--- server/routes/exportRoutes.js
+++ server/routes/exportRoutes.js
@@ -5,6 +5,7 @@ GET
 export/trips - export trips to csv or xlsx file
 export/entries - export journal entries to csv or xlsx file
 export/expenses - export expenses to csv or xlsx file
+export/budgets - export list of budgets
 */
 const express = require('express');
 const router = express.Router()
