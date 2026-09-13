080be91889b0a76ee8d134e8f963e0dd6dd68fee
Lara Klopper <laraK@corprecover.co.za>
2026-09-13 14:57:07 +0200

    changes

--- client/src/components/UpdateConversion.js
+++ client/src/components/UpdateConversion.js
@@ -6,12 +6,13 @@ import React from 'react'
 import '../css/componentCss/CurrencyConverter.css'
 import '../css/componentCss/FormSetup.css'
 import Stack from 'react-bootstrap/Stack';
-
+import Button from 'react-bootstrap/Button';
+import { Asterisk } from 'lucide-react';
 
 export default function UpdateConversion() {
   return (
-    <form id='currency-converter-form'>
-        <div id='formHeadingBlock' aria-describedby='formDescrip' >
+    <form id='currency-converter-form' method='PUT' aria-describedby='formDescrip'>
+        <div id='formHeaderBlock'>
             <Stack gap={3} id='formHeadingStack' >
       <div className="p-2 visually-hidden" >
         <p id='formDescrip'>Form to check if exchange Rate is the same and update the conversion</p>
@@ -22,14 +23,79 @@ export default function UpdateConversion() {
       <div className="p-2" id='editConvertMsgBlock'>
         <p className='editConverterMsg'>Update the conversion</p>
       </div>
+    </Stack>           
+        </div>
+        <div id='currency-converter-details'>
+            <Stack gap={3} id='converterStack1'>
+      <div className="p-2" id='convert-amount-block'>
+        <label className='converter-label'>AMOUNT:</label>
+        <div className='input-div'>
+            <input
+                className='input'
+                readOnly
+                // placeholder=''//currentAmount
+                // name=''
+                // value={}
+            />
+             <small><Asterisk color="#C22419" fontWeight={700} size={16} aria-hidden='true' focusable='false' /></small>
+        </div>
+      </div>
+      <div className="p-2" id='convert-baseCurrency-block'>
+        <label className='converter-label'>CONVERT FROM:</label>
+        <div className='input-div'>
+            <input
+                className='input'
+                readOnly
+                // placeholder=''//Current base currency
+                // name=''
+                // value={}
+                // ARIA ATTRIBUTES:
+            />
+              <small><Asterisk color="#C22419" fontWeight={700} size={16} aria-hidden='true' focusable='false' /></small>
+        </div>
+      </div>
+      <div className="p-2" id='convert-TargetCurrency-block'>
+        <label className='converter-label'>CONVERT TO:</label>
+        <div className='input-div'>
+<input
+            className='input'
+            readOnly
+            // placeholder=''//current Target currency
+            // name=''
+            // value={}
+            // ARIA ATTRIBUTES
+        />
+                     <small><Asterisk color="#C22419" fontWeight={700} size={16} aria-hidden='true' focusable='false' /></small>
+
+        </div>
+        
+      </div>
     </Stack>
 
-            
-        </div>
-        <div className='formTextBlock'>
 
         </div>
-
+<Stack gap={3} id='converter-stack2'>
+<div className="p-2" id='requiredInfo'>
+        <p className='infoText' aria-live='polite' aria-hidden='true'>
+            <small><Asterisk color="#C22419" fontWeight={700} size={16} aria-hidden='true' focusable='false' /> Indicates required information</small>
+        </p>
+    </div>
+      <div className="p-2" id='converter-btn-block1'>
+        <Button
+        variant='light'
+        id='convertCurrencyBtn'
+        type='submit'
+        // ARIA ATTRIBUTES
+        >
+            CONVERT
+        </Button>
+      </div>
+      {/* RESULT  BLOCK*/}
+      <div className="p-2" id='converterResultBlock' aria-live='polite'>
+        {/* save updated conversion button */}
+      </div>
+      {/* ERROR MESSAGE */}
+    </Stack>
     </form>
   )
 }
